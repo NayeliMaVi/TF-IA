c04e1eecda4799fc94a274f1280a7af3bdaddfc2
Nayeli <nmallma91@gmail.com>
2024-06-23 14:43:04 -0500

    TF-probando desplegable

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+from webscraping import perform_web_scraping
+from models import modelo
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process-link', methods=['POST'])
+
+def process_link():
+    url = request.form['jobLinkInput']
+    data = perform_web_scraping(url)    
+    print(data)
+    prueba = modelo.predict_new_entry(data['title'], data['location'], data['company_profile'], data['description'], data['employment_type'], data['required_experience'], data['job_function'])
+    return str(prueba)
+
+if __name__ == '__main__':
+    app.run(debug=True)
